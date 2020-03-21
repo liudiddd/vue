d7f2f38c3429e0a3c74eb5b794b5edf34eace703
liudidddd <609980188@qq.com>
2020-03-21 22:41:12 +0800

    添加router：/a

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,8 @@
 <script>
 export default {
   name: 'App'
+  
+  
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/a',
+      name: 'a',
+      component: HelloWorld
     }
   ]
 })
